36378ab6312fd51e6706677bff70f230725e3353
ejoun6718 <hjoun@ucsd.edu>
2018-08-30 21:33:27 -0700

    Add methods to manage users and fetch movie recommendations

--- sae.py
+++ sae.py
@@ -61,21 +61,21 @@ class SAE(nn.Module): # Stacked Autoencoder
         x = self.fc4(x)
         return x # Vector of predictive ratings
     # Predicts the user's rating for the chosen movie
-    def predict(self, target_user_id, target_movie_id):
-        input = Variable(training_set[target_user_id-1]).unsqueeze(0)
+    def predict(self, user_array, target_movie_id):
+        input = Variable(user_array).unsqueeze(0)
         output = sae(input)
         output_numpy = output.data.numpy()
         rating = output_numpy[0,target_movie_id-1]
         return rating
-    
 sae = SAE()
+
 # Criterion to update the weights - Loss function will be the Mean Squared Error
 criterion = nn.MSELoss()
 # Apply stochastic gradient descent to update the different weights to reduce the error at each epoch
 optimizer = optim.RMSprop(sae.parameters(), lr = 0.01, weight_decay = 0.5)
 
 # Training the SAE
-nb_epoch = 200
+nb_epoch = 5
 for epoch in range(1, nb_epoch + 1):
     train_loss = 0
     # Count the number of users who rated at least one movie
@@ -117,6 +117,70 @@ for id_user in range(nb_users):
         output[target == 0] = 0
         loss = criterion(output, target)
         mean_corrector = nb_movies/float(torch.sum(target.data > 0) + 1e-10)
-        test_loss += np.sqrt(loss.item()*mean_corrector)
+        test_loss += np.sqrt(loss.item() * mean_corrector)
         s += 1.
-print('test loss: '+str(test_loss/s))
+print('test loss: ' + str(test_loss/s))
+
+import collections
+def getPredictions(user_array, min_rating, max_rating, descending_order):
+    p_rating_list = collections.defaultdict(list)
+    for target_movie_id in range(nb_movies + 1):
+        p_rating = sae.predict(user_array, target_movie_id)
+        min_stars = int(p_rating) # Number of stars rounded down
+        if min_stars >= min_rating and min_stars <= max_rating:
+            movie_name = movies[1][target_movie_id]
+            if min_stars not in p_rating_list:
+                p_rating_list[min_stars] = [[movie_name, p_rating]]
+            else:
+                # Keep the list in order by rating
+                first_rating = p_rating_list[min_stars][0][1]
+                if (descending_order and p_rating > first_rating 
+                    or not descending_order and p_rating < first_rating):
+                    p_rating_list[min_stars].insert(0, [movie_name, p_rating])
+                else:
+                    p_rating_list[min_stars].append([movie_name, p_rating])
+    return p_rating_list
+
+def getGoodRecs(user_array):
+    return getPredictions(user_array, 4, 5, True)
+
+def getBadRecs(user_array):
+    return getPredictions(user_array, 1, 2, False)
+
+def getAverageRecs(user_array):
+    return getPredictions(user_array, 3, 3, True)
+
+def getAllRecs(user_array):
+    return getPredictions(user_array, 1, 5, True)
+
+users_array = []
+users = None
+
+def getMovieNames():
+    movie_name_list = []
+    for i, elem in enumerate(movies[1]):
+        movie_name_list.append(elem[:-7])
+    return movie_name_list
+
+def addUser():
+    global users
+    new_user = [0] * nb_movies
+    users_array.append(new_user)
+    users = torch.FloatTensor(users_array)
+
+def deleteUser(target_user_id):
+    global users
+    users_array.remove(target_user_id)
+    users = torch.FloatTensor(users_array)
+
+def addMovie(target_user_id, movie_name, rating):
+    global users
+    target_movie_id = -1
+    for i in range(nb_movies):
+        if movies[1][i][:-7] == movie_name:
+            target_movie_id = i
+            users_array[target_user_id][target_movie_id] = rating
+            users = torch.FloatTensor(users_array)
+            return target_movie_id
+    print("Movie not found")
+    return target_movie_id
